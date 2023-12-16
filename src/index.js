848cf89a8282c936ae20f34fa9d5a2f305349f46
ericadlamini <erica.thamie@gmail.com>
2023-12-16 15:58:01 +0200

    Added API Intregration

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temp-degrees");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
 function updateCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("h1#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
+}
+
+function searchCity(city) {
+  let apiKey = "1fea1990c3a20o02b5dea044tb336292";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", updateCity);
+
+searchCity("Cape Town");
